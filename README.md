b4fb5699fcc07468767a1d768f7aa0456db6b5a0
GVE Devnet Admin <gve-devnet.gen@cisco.com>
2021-05-11 13:30:07 +0000

    Baselined from internal Repository last_commit:c22fcbe6712d78a99ee760db589ad3e46c4b92b8

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # GVE_DevNet_Collab_Endpoint_PIN_Code
-Cisco Collaboration Endpoints macro that locks a device with a pin code as soon as it goes out of standby. This was inherited from [roomdevices-macros-samples/PinCodeLock](https://github.com/CiscoDevNet/roomdevices-macros-samples/tree/master/Pin%20Code%20Lock) with few enhancments on: 
+Cisco Collaboration Endpoints macro that sets up a pin code that locks a device. And asks for it as the device goes out of standby.  
+Parts of this prototype were inherited from:
+- Macros sample: Pin Code Lock: [roomdevices-macros-samples/PinCodeLock](https://github.com/CiscoDevNet/roomdevices-macros-samples/tree/master/Pin%20Code%20Lock) 
+- Environment variables for Macros: [ObjectIsAdvantag/macros-env](https://github.com/ObjectIsAdvantag/macros-env) 
+
+With added enhancments on: 
+- Showing a prompt asking the user to setup a pin code, or ask for it if it was configured
+- Storing the pin code to a local inactive macro acting like a database/environment
 - Limiting the times allowed to try the pin code
 - If the maximum number tries is reached, the device will be disabled for an amount of time 
-- If the device was disabled, it will display a count-down showing the remaining time 
+- If the device was disabled, it will display a count-down showing the remaining time in seconds
 
 
 ## Contacts
@@ -14,6 +21,7 @@ Cisco Collaboration Endpoints macro that locks a device with a pin code as soon 
 
 ## Installation/Configuration
 
+- Make sure that standby has not been disabled on your system, otherwise the pin prompt might not appear.
 The installation will be done by adding the Macro to the device and enabling it, as follows:  
 ![/IMAGES/menu.png](/IMAGES/menu.png)  
 Integration > Macro Editor > Create a new macro > paste the content of the [JavaScript file](/lock_with_PIN_code.js)
@@ -21,11 +29,10 @@ Integration > Macro Editor > Create a new macro > paste the content of the [Java
 
 ## Usage
 
-- Make sure that standby has not been disabled on your system, otherwise the pin prompt might not appear.
 - Once the macro is added to the macro editor, change the following variables in the macro:  
-    - Set up the pin number:  
+    - Set up the name of the local database_macro where variables are will be stored and persisted:
         ```JavaScript
-        const PIN_CODE = '1234';
+        const MACRO_DB_NAME = 'LOCAL_DB_PIN_CODE';
         ```
     - Set up the maximum number of tries:  
         ```JavaScript
@@ -35,13 +42,39 @@ Integration > Macro Editor > Create a new macro > paste the content of the [Java
         ```JavaScript
         const TIME_TO_DISABLE = 2; // In minutes
         ```
+    
+
+- Once the macro is enabled, the other local database_macro will be generated (if it doesn't exist) with the default values located at:
+    ```JavaScript
+    const DEFAULT_ENV = {
+        pin_is_configured: false,
+        pin_code: '1234'
+    }
+    ```
+- The local database_macro will act as the environment/database of the enabled one. Storing a json object having two variables; controlling the pin-code settings on the device:
+    ```JavaScript
+    const json = {"pin_is_configured":false,"pin_code":"1234"}
+    ```
+
+- If the user had their pin-code configured but forgot it or having troubles inserting it, they can ask the system admin to either change it manually on the database_macro, or reset the value of ```"pin_is_configured"``` to ```false``` for the user to be able to setup a new one.
+
+![/IMAGES/inactive_database_macro.png](/IMAGES/inactive_database_macro.png)
 
 # Screenshots
 
+- Setting up pin-code:
+![/IMAGES/setup_pin_code.png](/IMAGES/setup_pin_code.png)
+
+- Verifying the entered pin-code:
+![/IMAGES/verify_pin_code.png](/IMAGES/verify_pin_code.png)
+
+- Asking for pin-code:
 ![/IMAGES/enter_pin.png](/IMAGES/enter_pin.png)
 
+- Wrong pin-code was entered:
 ![/IMAGES/incorrect_pin.png](/IMAGES/incorrect_pin.png)
 
+- Maximum number of tries has been reached:
 ![/IMAGES/countdown.gif](/IMAGES/countdown.gif)
 
 ### LICENSE
--- lock_with_PIN_code.js
+++ lock_with_PIN_code.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 Cisco and/or its affiliates.
+Copyright (c) 2021 Cisco and/or its affiliates.
 
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
@@ -17,23 +17,185 @@ or implied.
 
 const xapi = require('xapi');
 
-const PIN_CODE = '1234';
 const MAX_TRIES = 3;
 const TIME_TO_DISABLE = 2; // In minutes
 
 // Counter to keep track of the number of tries
 var curr_tries = 1;
 
+// PIN code variable, to be setup by the user from a prompt window
+// Value is written & read from a local database_macro created below
+var pin_code;
+
+// Boolean to check if the pin code for device has been configure or not. 
+// Value is  written & read from a local database_macro created below
+var pin_is_configured;
+
 // Boolean to keep track if the device was disabled for entering the wrong PIN
-var device_disabled = false;
+var device_is_disabled = false;
 
 // Variable to keep track of the remaining time
 var remaining_time = 0; // In seconds
 
-// Show the window to ask for PIN code
-function askForPin(text = 'Enter PIN code:' + ' (' + curr_tries + '/' + MAX_TRIES + ')<br>(default:1234)') {
+// These variables are to be stored to a local inactive macro
+const DEFAULT_ENV = {
+    pin_is_configured: false,
+    pin_code: '1234'
+}
+
+//  Configure if the environment should be storing a volatile or persist environment variables into the inactive ENV macro
+//  - true for transient variables (any change will not survive Macro restart)
+//  - false for persisted variables (changes are persisted into a MACRO_DB_NAME file of a macro)
+let volatile = false;
+const MACRO_DB_NAME = 'LOCAL_DB_PIN_CODE'; // name of the database_macro where variables are persisted (if enabled)
+
+// Creating env variable to read/write data from/to the inactive database_macro
+let ENV;
+
+// Once the macro is started, initialize the environment of the inactive database_macro, or create it if it doesn't exist
+require('xapi').on('ready', async (xapi) => {
+
+    // Initialize environment
+    await initEnvironment(xapi);
+
+});
+
+// Initiating the environment
+async function initEnvironment(xapi) {
+
+    // Volatile mode
+    if (volatile) {
+        // List of variables for the local environment
+        console.info('Starting in volatile mode: environment variables are not persisted');
+        ENV = DEFAULT_ENV;
+        return;
+    }
+
+    // Persistent mode
+    console.info('Starting in persistent mode: environment variables are stored in ' + MACRO_DB_NAME + ' macro.');
+    let data;
+    try {
+        data = await read(xapi, true);
+    }
+    catch (err) {
+        if (err.message == 'DB_READ_ERROR') {
+            console.log("Cannot access the database_macro: " + MACRO_DB_NAME);
+        }
+        else {
+            console.info(`Unexpected read error while accessing DB: ${JSON.stringify(err.message)}`)
+        }
+    }
+
+    // if ENV is empty, create a new databe_macro with default ENV
+    if (!data) {
+        console.info('No existing database_macro. Creating the default environment at: ' + MACRO_DB_NAME);
+        ENV = DEFAULT_ENV;
+        try {
+            await write(xapi, ENV);
+        }
+        catch (err) {
+            console.debug(`Write error while creating DB: ${JSON.stringify(err.message)}`)
+            console.info('Changing to non-persistent mode.');
+            volatile = true;
+        }
+    }
+    else {
+        ENV = data;
+        // Update the pin-code variables from what was read from the inactive database_macro
+        pin_is_configured = ENV['pin_is_configured'];
+        pin_code = ENV['pin_code'];
+        console.log('PIN code data has been read from: ' + MACRO_DB_NAME);
+    }
+}
+
+// Database persistence into the inactive database_macro 
+const PREFIX = 'const json = ';
+
+// Read the database_macro contents
+async function read(xapi, env_is_available) {
+    // Load contents
+    let contents;
+    try {
+        let macro = await xapi.command('Macros Macro Get', { Name: MACRO_DB_NAME, Content: true })
+        contents = macro.Macro[0].Content.substring(PREFIX.length);
+    }
+    catch (err) {
+        if (!env_is_available) {
+            // Log error is ENV should exist
+            console.error(`Cannot load contents from macro: ${MACRO_DB_NAME}`);
+        }
+        throw new Error("DB_READ_ERROR");
+    }
+
+    // Parse contents
+    try {
+        console.debug(`DB contains: ${contents}`);
+        let data = JSON.parse(contents);
+        console.debug('DB successfully parsed: ' + contents);
+        return data;
+    }
+    catch (err) {
+        console.error('DB is corrupted, cannot JSON parse the DB');
+        throw new Error('DB_PARSE_ERROR');
+    }
+}
+
+// Write the database_macro contents
+async function write(xapi, data) {
+    // Serialize data as JSON and append prefix
+    let contents;
+    try {
+        contents = PREFIX + JSON.stringify(data);
+    }
+    catch (err) {
+        console.debug('Contents cannot be serialized to JSON');
+        throw new Error('DB_SERIALIZE_ERROR');
+    }
+
+    // Write
+    try {
+        let res = await xapi.command('Macros Macro Save', { Name: MACRO_DB_NAME, OverWrite: true, body: contents });
+        return (res.status == 'OK');
+    }
+    catch (err) {
+        if (err.message == 'Max number of macros reached.') {
+            console.error('Max number of macros reached. Please free up some space.');
+            throw new Error('DB_MACROS_LIMIT');
+        }
+
+        console.debug(`Cannot write contents to macro: ${MACRO_DB_NAME}`);
+        throw new Error('DB_WRITE_ERROR');
+    }
+}
+
+// Show the window to setup a new PIN code
+function setupPin(text = 'Hello!<br>Enter a new PIN code to lock this device:') {
+    console.log('Asking the user to setup a new pin..');
     xapi.command('UserInterface Message TextInput Display', {
-        FeedbackId: 'pin-code',
+        FeedbackId: 'setup-pin',
+        Text: text,
+        InputType: 'PIN',
+        Placeholder: ' ',
+        Duration: 0,
+    });
+}
+
+// Show the window to verify the entered PIN code
+function verifyPin(text = 'Enter the PIN code again to confirm:') {
+    console.log('Asking the user to verify the pin..');
+    xapi.command('UserInterface Message TextInput Display', {
+        FeedbackId: 'verify-pin',
+        Text: text,
+        InputType: 'PIN',
+        Placeholder: ' ',
+        Duration: 0,
+    });
+}
+
+// Show the window to ask for PIN code
+function askForPin(text = 'Enter PIN code:<br>(' + curr_tries + '/' + MAX_TRIES + ')') {
+    xapi.command('UserInterface Message TextInput Display', {
+        FeedbackId: 'check-pin',
         Text: text,
         InputType: 'PIN',
         Placeholder: ' ',
@@ -43,7 +205,7 @@ function askForPin(text = 'Enter PIN code:' + ' (' + curr_tries + '/' + MAX_TRIE
 
 // Alerting about the disablement of the device
 function alertDisabledDevice() {
-    var text = 'Device is disabled! Please contact support...';
+    var text = 'Device is disabled! Wait for the remaining time or contact support...';
     xapi.command('UserInterface Message TextInput Display', {
         FeedbackId: 'device-disabled-alert',
         Text: text + '<br>Remaining time: ' + remaining_time,
@@ -71,13 +233,13 @@ function resetTries() {
 }
 
 // Handling the response of the PIN entering prompt
-function onResponse(code) {
-    console.log('Try PIN', code);
-    if (code === PIN_CODE) {
+function checkPin(inserted_code) {
+    console.log('Try PIN', inserted_code);
+    if (inserted_code === pin_code) {
         console.log('PIN was accepted');
         resetTries();
     }
-    else if (code === '') {
+    else if (inserted_code === '') {
         console.log('Empty pin was inserted');
         askForPin();
     }
@@ -99,7 +261,7 @@ function onResponse(code) {
 // Disable the device after reaching the maximum number of tries
 function disable_device() {
     console.log('Disabling device for ' + TIME_TO_DISABLE + ' minute/s starting at: ' + getTime());
-    device_disabled = true;
+    device_is_disabled = true;
     remaining_time = (TIME_TO_DISABLE * 60);
 
     // Setting time to undisable the device after the remaining time ends
@@ -119,7 +281,7 @@ function disable_device() {
 // Undisable the device after the timeout is completed
 function undisable_device() {
     console.log('Device has been undisabled at: ' + getTime());
-    device_disabled = false;
+    device_is_disabled = false;
     resetTries();
     xapi.command('UserInterface Message TextInput Clear', {
         FeedbackId: 'device-disabled-alert',
@@ -130,36 +292,76 @@ function undisable_device() {
 function listenToEvents() {
     // when the user submits a TextInput form
     xapi.event.on('UserInterface Message TextInput Response', (event) => {
-        // If clicked OK on pin-code
-        if (event.FeedbackId === 'pin-code')
-            onResponse(event.Text);
+        // If clicked OK on check-pin
+        if (event.FeedbackId === 'check-pin')
+            checkPin(event.Text);
         // Else if clicked OK on disabled-device-alert
         else if (event.FeedbackId === 'device-disabled-alert') {
-            if (device_disabled) alertDisabledDevice();
+            if (device_is_disabled) alertDisabledDevice();
             else askForPin();
             xapi.command('Standby Halfwake');
+        }
+        else if (event.FeedbackId === 'setup-pin') {
+            if(event.Text === '') {
+                console.log('Empty pin was inputted..');
+                setupPin('PIN code can\'t be empty!<br>Enter a new PIN code to lock this device:');
+            }
+            else {
+                console.log('PIN code is setup to be: ' + event.Text);
+                pin_code = event.Text;
+                verifyPin();
+            }
+        }
+        else if (event.FeedbackId === 'verify-pin') {
+            if (event.Text != pin_code) {
+                console.log('Verification pin is wrong..');
+                setupPin('The code you entered doesn\'t match!<br>Enter a new PIN code to lock this device:');
+            }
+            else {
+                console.log('Verification completed.. pin code is configured: ' + pin_code);
+                pin_is_configured = true;
+                ENV['pin_is_configured'] = true;
+                ENV['pin_code'] = pin_code;
+                try {
+                    write(xapi, ENV);
+                }
+                catch (err) {
+                    console.debug(`Write error while updating DB: ${JSON.stringify(err.message)}`);
+                }
+            }
         }
     });
     // when the user cancels a TextInput form
     xapi.event.on('UserInterface Message TextInput Clear', (event) => {
-        // If clicked cancel on pin-code
-        if (event.FeedbackId === 'pin-code') {
+        // If clicked cancel on check-pin
+        if (event.FeedbackId === 'check-pin') {
             askForPin();
             xapi.command('Standby Halfwake');
         }
         // Else if clicked cancel on disabled-device-alert
         else if (event.FeedbackId === 'device-disabled-alert') {
-            if (device_disabled) alertDisabledDevice();
+            if (device_is_disabled) alertDisabledDevice();
             else askForPin();
             xapi.command('Standby Halfwake');
+        }
+        // Else if clicked cancel on setup-pin
+        else if (event.FeedbackId === 'setup-pin') {
+            console.log('Setting up PIN was canceled..');
+        }
+        // Else if clicked cancel on verify-pin
+        else if (event.FeedbackId === 'verify-pin') {
+            console.log('Verification canceled..');
         }
     });
     // when the device changes its Standby State
     xapi.status.on('Standby State', (state) => {
         // When the device is awaken from Standby State
         if (state === 'Off') {
-            if (device_disabled) alertDisabledDevice();
-            else askForPin();
+            if (device_is_disabled) alertDisabledDevice();
+            else {
+                if (pin_is_configured) askForPin();
+                else setupPin();
+            }
         }
     });
 }
